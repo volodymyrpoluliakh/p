9ecdce9ede6edf90c9c940ebbf86d44b2e5a7eea
volodymyrpoluliakh <38631195+volodymyrpoluliakh@users.noreply.github.com>
2022-03-02 13:32:53 +0200

    change workdir

--- install_mhddos_gcp.sh
+++ install_mhddos_gcp.sh
@@ -5,7 +5,7 @@ set -eu
 sudo apt update
 sudo apt install git build-essential libssl-dev libffi-dev python3-dev python3-pip cargo -y
 
-git clone https://github.com/volodymyrpoluliakh/p
+pushd $HOME
 
 git clone https://github.com/MHProDev/MHDDoS.git
 pushd $(pwd)/MHDDoS
