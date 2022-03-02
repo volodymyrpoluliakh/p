3c2a75a8f50b48609ae63947c0e1be48123ad308
volodymyrpoluliakh <38631195+volodymyrpoluliakh@users.noreply.github.com>
2022-03-02 13:31:08 +0200

    install_mhddos_gcp.sh

---
+++ install_mhddos_gcp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+sudo apt update
+sudo apt install git build-essential libssl-dev libffi-dev python3-dev python3-pip cargo -y
+
+git clone https://github.com/volodymyrpoluliakh/p
+
+git clone https://github.com/MHProDev/MHDDoS.git
+pushd $(pwd)/MHDDoS
+pip3 install -r requirements.txt
+popd
